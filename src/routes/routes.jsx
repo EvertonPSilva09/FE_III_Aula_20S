335e967f040536fafc8139585fa3e9890e5f4ec8
Everton Silva <everton.p.silva@outlook.com>
2023-04-02 20:43:49 -0300

    Adicionando rotas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,3 +1,20 @@
 import React from "react";
-import { Route, BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "../pages/home";
+import Login from "../pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+export default function Routes() {
+  return <RouterProvider router={router} />;
+}
